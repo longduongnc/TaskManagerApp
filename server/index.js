d1444acb6cf4fe8aec34b6a56de506d33fbeffe7
Long Duong <longduongnc@gmail.com>
2024-11-29 15:14:35 +0700

    Inittial commit: Task Manager App setup

---
+++ server/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Error connecting to MongoDB:', err));
+
+
